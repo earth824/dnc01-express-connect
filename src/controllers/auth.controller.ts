3ef859a7a50a1326c75d6a71b8b82b96f398ec99
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2026-01-30 15:42:26 +0700

    register validate and sent request to server

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,9 @@
 import type { RequestHandler } from 'express';
 
 const register: RequestHandler = (req, res) => {
-  res.status(201).json({ message: 'registered successfully' });
+  setTimeout(() => {
+    res.status(201).json({ message: 'registered successfully' });
+  }, 5000);
 };
 
 export const authController = { register };
